41a23be399b3d92d57bbe57f9f73292279861239
Andrey05319 <65806981+Andrey05319@users.noreply.github.com>
2022-12-16 13:55:53 +0600

    Update 2022_12_16_13_55

--- 22_Pong_Game/ball.py
+++ 22_Pong_Game/ball.py
@@ -2,6 +2,8 @@ from turtle import Turtle
 import time
 
 STARTING_COORDINATES = (0, 0)
+increment_x = 10
+increment_y = 10
 
 
 class Ball(Turtle):
@@ -15,9 +17,13 @@ class Ball(Turtle):
         self.goto(STARTING_COORDINATES)
 
     def move(self):
-        increment_x = 1
-        increment_y = 1
         new_x = self.xcor() + increment_x
         new_y = self.ycor() + increment_y
+        if (new_y > 290) or (new_y < -290):
+            print("hehehe")
+            increment_y = increment_y * (-1)
+            new_y = self.ycor() + increment_y
+
+
         time.sleep(0.1)
         self.goto(new_x, new_y)
