2bf1eceac3b1cef642fa0a706968b9c4c9992f57
Andrey05319 <65806981+Andrey05319@users.noreply.github.com>
2022-12-16 13:12:58 +0600

    Update 2022_12_16_13_12

--- 22_Pong_Game/main22.py
+++ 22_Pong_Game/main22.py
@@ -1,28 +1,23 @@
 from turtle import Screen
 from paddle import Paddle
 
-
 screen = Screen()
 screen.setup(width=800, height=600)
 screen.bgcolor("black")
 screen.title("Andrey's Pong Game")
-screen.tracer(0) # Turning off the animation of moving
+screen.tracer(0)  # Turning off the animation of moving
 
-paddle_right = Paddle()
-
+r_paddle = Paddle((350, 0))
+l_paddle = Paddle((-350, 0))
 
 screen.listen()
-screen.onkey(paddle_right.up, "Up")
-screen.onkey(paddle_right.down, "Down")
+screen.onkey(r_paddle.up, "Up")
+screen.onkey(r_paddle.down, "Down")
+screen.onkey(l_paddle.up, "q")
+screen.onkey(l_paddle.down, "a")
 
 game_is_on = True
 while game_is_on:
-    screen.update() # Works in pair with screen.tracer(0) - updating the screen in absence of animation
-
-
-
-
-
-
+    screen.update()  # Works in pair with screen.tracer(0) - updating the screen in absence of animation
 
 screen.exitonclick()
--- 22_Pong_Game/paddle.py
+++ 22_Pong_Game/paddle.py
@@ -1,18 +1,16 @@
 from turtle import Turtle
 
-STARTING_COORDINATES = (350, 0)
-
 
 class Paddle(Turtle):
 
-    def __init__(self):
+    def __init__(self, starting_coordinates):
         super().__init__()
         self.shape("square")
         self.penup()
         self.shapesize(stretch_len=1, stretch_wid=5)
         self.color("white")
         self.speed("fastest")
-        self.goto(STARTING_COORDINATES)
+        self.goto(starting_coordinates)
 
     def up(self):
         if self.ycor() < 250:
